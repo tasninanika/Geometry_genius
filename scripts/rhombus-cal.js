f85f444d01b0921cfb96e49fd0bdeaec7aac66ba
anikatasnin <jarintasnin09@gmail.com>
2024-08-22 15:22:41 +0000

    rohmbus card added

---
+++ scripts/rhombus-cal.js
@@ -0,0 +1,15 @@
+function calrhoArea(){
+    // get base value
+    const rhoD1 = document.getElementById('rho-d1');
+    const rhoD1text = rhoD1.value;
+    const D1 = parseFloat(rhoD1text);
+
+    // get height value
+    const rhoD2 = document.getElementById('rho-d2');
+    const rhoD2text = rhoD2.value;
+    const D2 = parseFloat(rhoD2text);
+    
+    // calculate area
+    const area = 0.5 * D1 * D2;
+    console.log(area);
+}
